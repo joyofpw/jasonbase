3896cf3cccedc2c14c99f578e3252f39413d050a
Camilo <camilo@ninjas.cl>
2019-02-23 23:38:32 -0300

    added pw tags

--- pw/site/templates/views/includes/footer.php
+++ pw/site/templates/views/includes/footer.php
@@ -5,8 +5,13 @@
     
     <div class="columns">
         <div class="column has-text-centered">
-            <p><strong>Jasonbase</strong> is a proud member of the <a href="https://jasonelle.com">Jasonelle</a> family.</p>
+            <p><strong><a href="https://base.jasonelle.com">Jasonbase</a></strong> is a proud member of the <a href="https://jasonelle.com">Jasonelle</a> family.</p>
+            <br>
+            <p>
+                <a id='processwire' target='_blank' href='http://processwire.com'>Powered by ProcessWire Open Source CMS/CMF</a>
+            </p>
         </div>
+        
     </div>
 
     <div class="block"></div>
--- pw/site/templates/views/includes/header.php
+++ pw/site/templates/views/includes/header.php
@@ -3,6 +3,7 @@
 <!DOCTYPE html>
 <head>
     <meta charset="utf-8">
+    <meta name="generator" content="ProcessWire 3.0.x">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo $paths->node ?>bulma/css/bulma.min.css">
 </head>
